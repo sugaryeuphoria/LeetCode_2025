d662580c1a0fe6d406d77805f61a1133e6362ce2
Pooja Verma <pooja32verma@gmail.com>
2025-01-01 21:51:08 -0800

    Constraints

--- 2559_CountVowelStringsInRanges.py
+++ 2559_CountVowelStringsInRanges.py
@@ -18,4 +18,13 @@ Example 2:
 Input: words = ["a","e","i"], queries = [[0,2],[0,1],[2,2]]
 Output: [3,2,1]
 Explanation: Every string satisfies the conditions, so we return [3,2,1].
+
+Constraints:
+
+1 <= words.length <= 105
+1 <= words[i].length <= 40
+words[i] consists only of lowercase English letters.
+sum(words[i].length) <= 3 * 105
+1 <= queries.length <= 105
+0 <= li <= ri < words.length
 """
