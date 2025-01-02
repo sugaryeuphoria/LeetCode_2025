25fe1b9a01a8cec102a7d1719b773f0932e7554c
Pooja Verma <pooja32verma@gmail.com>
2025-01-01 21:53:39 -0800

    Process the queries

--- 2559_CountVowelStringsInRanges.py
+++ 2559_CountVowelStringsInRanges.py
@@ -39,3 +39,10 @@ class Solution(object):
         prefix = [0] * (n + 1)
         for i in range(n):
             prefix[i + 1] = prefix[i] + (1 if is_vowel_string(words[i]) else 0)
+        
+        # Process the queries
+        result = []
+        for li, ri in queries:
+            result.append(prefix[ri + 1] - prefix[li])
+        
+        return result
