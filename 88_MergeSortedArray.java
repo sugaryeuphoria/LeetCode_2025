0da4f6b5dfde704408f61915651de9458421e116
Pooja Verma <pooja32verma@gmail.com>
2025-01-06 15:27:15 -0800

    Condition when p2 > p1

--- 88_MergeSortedArray.java
+++ 88_MergeSortedArray.java
@@ -52,7 +52,11 @@ class Solution {
             if(nums1[p1] > nums2[p2]){
                 nums1[p] = nums1[p1];
                 p1--;
+            }else{
+                nums1[p] = nums2[p2];
+                p2--;
             }
+            p--;
         }
     }
 }
