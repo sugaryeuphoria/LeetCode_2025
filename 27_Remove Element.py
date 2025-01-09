39a82ed9236faf5274dcc505d7fb13372f625c07
Pooja Verma <pooja32verma@gmail.com>
2025-01-09 10:35:25 -0800

    Constraints

--- 27_Remove Element.py
+++ 27_Remove Element.py
@@ -35,4 +35,10 @@ Output: 5, nums = [0,1,4,0,3,_,_,_]
 Explanation: Your function should return k = 5, with the first five elements of nums containing 0, 0, 1, 3, and 4.
 Note that the five elements can be returned in any order.
 It does not matter what you leave beyond the returned k (hence they are underscores).
+
+Constraints:
+
+0 <= nums.length <= 100
+0 <= nums[i] <= 50
+0 <= val <= 100
 """
