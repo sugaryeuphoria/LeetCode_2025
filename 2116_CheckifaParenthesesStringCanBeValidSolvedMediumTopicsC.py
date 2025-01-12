d894502f2a9e3b8690dcdc5fe2f6099a51cd7f4b
Pooja Verma <pooja32verma@gmail.com>
2025-01-11 20:28:36 -0800

    Right to left pass

--- 2116_CheckifaParenthesesStringCanBeValidSolvedMediumTopicsC.py
+++ 2116_CheckifaParenthesesStringCanBeValidSolvedMediumTopicsC.py
@@ -70,3 +70,18 @@ class Solution(object):
             # Ensure at no point ')' exceeds '(' + flexible
             if close_count > open_count + flexible:
                 return False
+             # Right to left pass
+        open_count, close_count, flexible = 0, 0, 0
+        for i in range(len(s) - 1, -1, -1):
+            if locked[i] == "1":  # Locked character
+                if s[i] == '(':
+                    open_count += 1
+                else:
+                    close_count += 1
+            else:  # Unlocked character
+                flexible += 1
+            
+            # Ensure at no point '(' exceeds ')' + flexible
+            if open_count > close_count + flexible:
+                return False
+        
