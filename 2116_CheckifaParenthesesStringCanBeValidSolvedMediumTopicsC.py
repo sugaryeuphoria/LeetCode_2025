715fe789a8c4e23990c35076a34a099f68a1d43b
Pooja Verma <pooja32verma@gmail.com>
2025-01-11 20:25:43 -0800

    Example use cases

--- 2116_CheckifaParenthesesStringCanBeValidSolvedMediumTopicsC.py
+++ 2116_CheckifaParenthesesStringCanBeValidSolvedMediumTopicsC.py
@@ -10,4 +10,24 @@ You are given a parentheses string s and a string locked, both of length n. lock
 If locked[i] is '1', you cannot change s[i].
 But if locked[i] is '0', you can change s[i] to either '(' or ')'.
 Return true if you can make s a valid parentheses string. Otherwise, return false.
+
+Example 1:
+
+
+Input: s = "))()))", locked = "010100"
+Output: true
+Explanation: locked[1] == '1' and locked[3] == '1', so we cannot change s[1] or s[3].
+We change s[0] and s[4] to '(' while leaving s[2] and s[5] unchanged to make s valid.
+Example 2:
+
+Input: s = "()()", locked = "0000"
+Output: true
+Explanation: We do not need to make any changes because s is already valid.
+Example 3:
+
+Input: s = ")", locked = "0"
+Output: false
+Explanation: locked permits us to change s[0]. 
+Changing s[0] to either '(' or ')' will not make s valid.
+ 
 """
