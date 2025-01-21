2678e433ae602b06f42c304882c319f52516981d
Pooja Verma <pooja32verma@gmail.com>
2025-01-20 22:12:48 -0800

    Constraints

--- 2661_FirstCompletelyPaintedRoworColumn.py
+++ 2661_FirstCompletelyPaintedRoworColumn.py
@@ -17,4 +17,14 @@ Input: arr = [2,8,7,4,1,3,5,6,9], mat = [[3,2,5],[1,4,6],[8,7,9]]
 Output: 3
 Explanation: The second column becomes fully painted at arr[3].
  
+Constraints:
+
+m == mat.length
+n = mat[i].length
+arr.length == m * n
+1 <= m, n <= 105
+1 <= m * n <= 105
+1 <= arr[i], mat[r][c] <= m * n
+All the integers of arr are unique.
+All the integers of mat are unique.
 """
