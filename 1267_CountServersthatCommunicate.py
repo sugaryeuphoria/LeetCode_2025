1ec55c7146359da4701560e19b5cabc7133f1c95
Pooja Verma <pooja32verma@gmail.com>
2025-01-22 23:32:04 -0800

    Count servers that can communicate

--- 1267_CountServersthatCommunicate.py
+++ 1267_CountServersthatCommunicate.py
@@ -47,3 +47,12 @@ class Solution(object):
                 if grid[i][j] == 1:
                     row_count[i] += 1
                     col_count[j] += 1
+
+        # Count servers that can communicate
+        total_servers = 0
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == 1 and (row_count[i] > 1 or col_count[j] > 1):
+                    total_servers += 1
+        
+        return total_servers
