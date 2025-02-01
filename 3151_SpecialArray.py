85cd076245741115e395ecc38d2366e9ff4719bb
Pooja Verma <pooja32verma@gmail.com>
2025-02-01 15:03:40 -0800

    Problem to solve

---
+++ 3151_SpecialArray.py
@@ -0,0 +1,6 @@
+"""
+3151. Special Array I
+An array is considered special if every pair of its adjacent elements contains two numbers with different parity.
+
+You are given an array of integers nums. Return true if nums is a special array, otherwise, return false.
+"""
