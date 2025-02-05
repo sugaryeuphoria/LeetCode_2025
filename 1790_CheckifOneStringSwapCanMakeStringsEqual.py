8aae7a14d6072a98d43b2fd7983620439f9def49
Pooja Verma <pooja32verma@gmail.com>
2025-02-04 21:49:08 -0800

    Problem

--- 1790_CheckifOneStringSwapCanMakeStringsEqual.py
+++ 1790_CheckifOneStringSwapCanMakeStringsEqual.py
@@ -1,3 +1,7 @@
 """
 1790. Check if One String Swap Can Make Strings Equal
+You are given two strings s1 and s2 of equal length. A string swap is an operation where you choose two indices in a string (not necessarily different) and swap the characters at these indices.
+
+Return true if it is possible to make both strings equal by performing at most one string swap on exactly one of the strings. Otherwise, return false.
+
 """
