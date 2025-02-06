175f263b2f4a4cb586fa346865a622adfd2e09d2
Pooja Verma <pooja32verma@gmail.com>
2025-02-05 22:07:35 -0800

    Iterate through all pairs of numbers

--- 1726_TuplewithSameProduct.py
+++ 1726_TuplewithSameProduct.py
@@ -31,3 +31,9 @@ class Solution:
     def tupleSameProduct(self, nums):
         product_count = defaultdict(int)
         result = 0
+        # Iterate through all pairs of numbers
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                product = nums[i] * nums[j]
+                result += product_count[product]
+                product_count[product] += 1
