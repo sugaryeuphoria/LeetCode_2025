204b9d43cb3f11e45e7e65b4aa32a0f6c0b8e96d
Pooja Verma <pooja32verma@gmail.com>
2025-02-06 21:06:36 -0800

    Example use cases

--- DistinctColors.py
+++ DistinctColors.py
@@ -7,4 +7,34 @@ There are limit + 1 balls with distinct labels in the range [0, limit]. Initiall
 Return an array result of length n, where result[i] denotes the number of distinct colors after ith query.
 
 Note that when answering a query, lack of a color will not be considered as a color.
+
+Example 1:
+
+Input: limit = 4, queries = [[1,4],[2,5],[1,3],[3,4]]
+
+Output: [1,2,2,3]
+
+Explanation:
+
+
+
+After query 0, ball 1 has color 4.
+After query 1, ball 1 has color 4, and ball 2 has color 5.
+After query 2, ball 1 has color 3, and ball 2 has color 5.
+After query 3, ball 1 has color 3, ball 2 has color 5, and ball 3 has color 4.
+Example 2:
+
+Input: limit = 4, queries = [[0,1],[1,2],[2,2],[3,4],[4,5]]
+
+Output: [1,2,2,3,4]
+
+Explanation:
+
+
+
+After query 0, ball 0 has color 1.
+After query 1, ball 0 has color 1, and ball 1 has color 2.
+After query 2, ball 0 has color 1, and balls 1 and 2 have color 2.
+After query 3, ball 0 has color 1, balls 1 and 2 have color 2, and ball 3 has color 4.
+After query 4, ball 0 has color 1, balls 1 and 2 have color 2, ball 3 has color 4, and ball 4 has color 5.
 """
